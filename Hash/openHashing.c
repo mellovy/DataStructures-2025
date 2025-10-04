0261e1bbeb8c6e580bd8bc1224147ce342295685
mellovy <22100684@usc.edu.ph>
2025-10-04 20:44:04 +0800

    Update openHashing.c

--- Hash/openHashing.c
+++ Hash/openHashing.c
@@ -1,19 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX 10
 
-typedef int Dictionary[MAX];
+typedef struct node {
+	int data;
+	struct node* next;
+} *SET;
 
-int hash(int key){
-    return key % MAX;
-}
+typedef SET Dictionary[MAX];
+
+void initDict(Dictionary D);
+void insert(Dictionary D, int data);
+void display(Dictionary D);
 
 int main(){
-    
-    Dictionary D = {0, 20, 30, 13, 33, 45, 28, 48, 108};
+	Dictionary D;
+	
+	initDict(D);
+	
+	insert(D, 0);
+	insert(D, 20);
+	insert(D, 30);
+	insert(D, 13);
+	insert(D, 33);
+	insert(D, 45);
+	insert(D, 28);
+	insert(D, 48);
+	insert(D, 108);
+	
+	display(D);
+	
+	return 0;
+}
 
-    for(int i = 0; i < MAX - 1; i++){
-        printf("Where is %d in the dictionary?\n", D[i]);
-        printf("Index: %d\n\n", hash(D[i]));
-    }
-    
+void initDict(Dictionary D){
+	int i = 0;
+	
+	for(i = 0; i < MAX; i++){
+		D[i] = NULL;
+	}
+}
+
+void insert(Dictionary D, int data){
+	SET newNode = (SET)malloc(sizeof(struct node));
+	int temp = data % 10;
+	
+	if(newNode != NULL){
+		newNode->data = data;
+		newNode->next = D[temp];
+		D[temp] = newNode;
+	}
+}
+
+void display(Dictionary D){
+	int i;
+	SET trav;
+	
+	for(i = 0; i < MAX; i++){
+		printf("Index [%d]: ", i);
+		
+		for(trav = D[i]; trav != NULL; trav = trav->next){
+			printf("%d -> ", trav->data);
+		}
+		
+		printf("NULL\n");
+	}
 }
